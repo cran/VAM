3cd97a99dedc2a31c5d4a29aabad4ae18944fa67
H. Robert Frost <rob.frost@dartmouth.edu>
2021-03-15 04:10:02 +0000

    version 0.5.2

--- inst/doc/VAM_pbmc_small.R
+++ inst/doc/VAM_pbmc_small.R
@@ -1,59 +1,81 @@
 ### R code from vignette source 'VAM_pbmc_small.Rnw'
 
 ###################################################
-### code chunk number 1: VAM_pbmc_small.Rnw:16-20
+### code chunk number 1: VAM_pbmc_small.Rnw:15-16
 ###################################################
 library(VAM)
-if (!requireNamespace("Seurat", quietly=TRUE)) {
-   stop("Seurat package not available!")
-}
 
 
 ###################################################
-### code chunk number 2: VAM_pbmc_small.Rnw:27-31
+### code chunk number 2: VAM_pbmc_small.Rnw:23-31
 ###################################################
-SeuratObject::pbmc_small
-gene.names = rownames(SeuratObject::pbmc_small)
-gene.names[1:5]
-Seurat::VariableFeatures(SeuratObject::pbmc_small)[1:5]
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	SeuratObject::pbmc_small
+	gene.names = rownames(SeuratObject::pbmc_small)
+	gene.names[1:5]
+	Seurat::VariableFeatures(SeuratObject::pbmc_small)[1:5]
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+}	
 
 
 ###################################################
-### code chunk number 3: VAM_pbmc_small.Rnw:38-50
+### code chunk number 3: VAM_pbmc_small.Rnw:38-54
 ###################################################
-gene.set.name = "Test"
-gene.ids = c("PPBP", "IGLL5", "VDAC3", "CD1C", "AKR1C3")
-# Create a collection list for this gene set
-gene.set.id.list = list()
-gene.set.id.list[[1]] = gene.ids
-names(gene.set.id.list)[1] = gene.set.name
-gene.set.id.list
-# Create the list of gene indices required by vamForSeurat()
-(gene.set.collection = createGeneSetCollection(gene.ids=gene.names,
-	gene.set.collection=gene.set.id.list))
-gene.indices = gene.set.collection[[1]]
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	gene.set.name = "Test"
+	gene.ids = c("PPBP", "IGLL5", "VDAC3", "CD1C", "AKR1C3")
+	# Create a collection list for this gene set
+	gene.set.id.list = list()
+	gene.set.id.list[[1]] = gene.ids
+	names(gene.set.id.list)[1] = gene.set.name
+	gene.set.id.list
+	# Create the list of gene indices required by vamForSeurat()
+	(gene.set.collection = createGeneSetCollection(gene.ids=gene.names,
+		gene.set.collection=gene.set.id.list))
+	gene.indices = gene.set.collection[[1]]
 (gene.names = gene.names[gene.indices])
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+}	
 
 
 ###################################################
-### code chunk number 4: VAM_pbmc_small.Rnw:57-60
+### code chunk number 4: VAM_pbmc_small.Rnw:61-68
 ###################################################
-pbmc.vam = vamForSeurat(seurat.data=SeuratObject::pbmc_small,
-    gene.set.collection=gene.set.collection,
-    center=F, gamma=T, sample.cov=F, return.dist=T)
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	pbmc.vam = vamForSeurat(seurat.data=SeuratObject::pbmc_small,
+	    gene.set.collection=gene.set.collection,
+	    center=F, gamma=T, sample.cov=F, return.dist=T)
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+}	
 
 
 ###################################################
-### code chunk number 5: VAM_pbmc_small.Rnw:65-67
+### code chunk number 5: VAM_pbmc_small.Rnw:73-79
 ###################################################
-pbmc.vam@assays$VAMdist[1,1:10]
-pbmc.vam@assays$VAMcdf[1,1:10]
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	pbmc.vam@assays$VAMdist[1,1:10]
+	pbmc.vam@assays$VAMcdf[1,1:10]
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+}	
 
 
 ###################################################
-### code chunk number 6: VAM_pbmc_small.Rnw:74-76
+### code chunk number 6: VAM_pbmc_small.Rnw:86-97
 ###################################################
-Seurat::DefaultAssay(object = pbmc.vam) = "VAMcdf"
-Seurat::FeaturePlot(pbmc.vam, reduction="tsne", features=gene.set.name)
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	Seurat::DefaultAssay(object = pbmc.vam) = "VAMcdf"
+	Seurat::FeaturePlot(pbmc.vam, reduction="tsne", features=gene.set.name)
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+	par(mar = c(0,0,0,0))
+	plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+	text(x = 0.5, y = 0.5,paste("Seurat package not available!\n",
+					 "FeaturePlot not generated."),
+	cex = 1.6, col = "black")
+}	
 
 
--- inst/doc/VAM_pbmc_small_sctransform.R
+++ inst/doc/VAM_pbmc_small_sctransform.R
@@ -1,68 +1,94 @@
 ### R code from vignette source 'VAM_pbmc_small_sctransform.Rnw'
 
 ###################################################
-### code chunk number 1: VAM_pbmc_small_sctransform.Rnw:15-19
+### code chunk number 1: VAM_pbmc_small_sctransform.Rnw:15-16
 ###################################################
 library(VAM)
-if (!requireNamespace("Seurat", quietly=TRUE)) {
-   stop("Seurat package not available!")
+
+
+###################################################
+### code chunk number 2: VAM_pbmc_small_sctransform.Rnw:23-30
+###################################################
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	SeuratObject::pbmc_small
+	gene.names = rownames(SeuratObject::pbmc_small)
+	gene.names[1:5]
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
 }
 
 
 ###################################################
-### code chunk number 2: VAM_pbmc_small_sctransform.Rnw:26-29
+### code chunk number 3: VAM_pbmc_small_sctransform.Rnw:35-44
 ###################################################
-SeuratObject::pbmc_small
-gene.names = rownames(SeuratObject::pbmc_small)
-gene.names[1:5]
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	pbmc_sctransform = Seurat::SCTransform(SeuratObject::pbmc_small, verbose=F)
+	# Compute PCA and UMAP on the normalized values
+	pbmc_sctransform = Seurat::RunPCA(pbmc_sctransform, npcs=10)
+	pbmc_sctransform = Seurat::RunUMAP(pbmc_sctransform, dims = 1:10) 
+	Seurat::VariableFeatures(pbmc_sctransform)[1:5]
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+}
 
 
 ###################################################
-### code chunk number 3: VAM_pbmc_small_sctransform.Rnw:34-39
+### code chunk number 4: VAM_pbmc_small_sctransform.Rnw:51-67
 ###################################################
-pbmc_sctransform = Seurat::SCTransform(SeuratObject::pbmc_small, verbose=F)
-# Compute PCA and UMAP on the normalized values
-pbmc_sctransform = Seurat::RunPCA(pbmc_sctransform, npcs=10)
-pbmc_sctransform = Seurat::RunUMAP(pbmc_sctransform, dims = 1:10) 
-Seurat::VariableFeatures(pbmc_sctransform)[1:5]
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	gene.set.name = "Test"
+	gene.ids = c("NKG7", "PPBP", "GNLY", "PF4", "GNG11")
+	# Create a collection list for this gene set
+	gene.set.id.list = list()
+	gene.set.id.list[[1]] = gene.ids
+	names(gene.set.id.list)[1] = gene.set.name
+	gene.set.id.list
+	# Create the list of gene indices required by vamForSeurat()
+	(gene.set.collection = createGeneSetCollection(gene.ids=gene.names,
+		gene.set.collection=gene.set.id.list))
+	gene.indices = gene.set.collection[[1]]
+	(gene.names = gene.names[gene.indices])
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+}
 
 
 ###################################################
-### code chunk number 4: VAM_pbmc_small_sctransform.Rnw:46-58
+### code chunk number 5: VAM_pbmc_small_sctransform.Rnw:74-81
 ###################################################
-gene.set.name = "Test"
-gene.ids = c("NKG7", "PPBP", "GNLY", "PF4", "GNG11")
-# Create a collection list for this gene set
-gene.set.id.list = list()
-gene.set.id.list[[1]] = gene.ids
-names(gene.set.id.list)[1] = gene.set.name
-gene.set.id.list
-# Create the list of gene indices required by vamForSeurat()
-(gene.set.collection = createGeneSetCollection(gene.ids=gene.names,
-	gene.set.collection=gene.set.id.list))
-gene.indices = gene.set.collection[[1]]
-(gene.names = gene.names[gene.indices])
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	pbmc.vam = vamForSeurat(seurat.data=pbmc_sctransform,
+	    gene.set.collection=gene.set.collection,
+	    center=F, gamma=T, sample.cov=F, return.dist=T)
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+}
 
 
 ###################################################
-### code chunk number 5: VAM_pbmc_small_sctransform.Rnw:65-68
+### code chunk number 6: VAM_pbmc_small_sctransform.Rnw:86-92
 ###################################################
-pbmc.vam = vamForSeurat(seurat.data=pbmc_sctransform,
-    gene.set.collection=gene.set.collection,
-    center=F, gamma=T, sample.cov=F, return.dist=T)
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	pbmc.vam@assays$VAMdist[1,1:10]
+	pbmc.vam@assays$VAMcdf[1,1:10]
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+}
 
 
 ###################################################
-### code chunk number 6: VAM_pbmc_small_sctransform.Rnw:73-75
+### code chunk number 7: VAM_pbmc_small_sctransform.Rnw:99-110
 ###################################################
-pbmc.vam@assays$VAMdist[1,1:10]
-pbmc.vam@assays$VAMcdf[1,1:10]
-
-
-###################################################
-### code chunk number 7: VAM_pbmc_small_sctransform.Rnw:82-84
-###################################################
-Seurat::DefaultAssay(object = pbmc.vam) = "VAMcdf"
-Seurat::FeaturePlot(pbmc.vam, reduction="tsne", features=gene.set.name)
+if (requireNamespace("Seurat", quietly=TRUE)) {
+	Seurat::DefaultAssay(object = pbmc.vam) = "VAMcdf"
+	Seurat::FeaturePlot(pbmc.vam, reduction="tsne", features=gene.set.name)
+} else {
+	message("Seurat package not available! Not executing associated vignette content.")
+		par(mar = c(0,0,0,0))
+	plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+	text(x = 0.5, y = 0.5,paste("Seurat package not available!\n",
+					 "FeaturePlot not generated."),
+	cex = 1.6, col = "black")
+}
 
 
